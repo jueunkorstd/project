b4edd3d70ef5caf295ca610513af7d8442beb2bd
JUEUN <99578411+jueunkorstd@users.noreply.github.com>
2023-06-08 01:11:52 +0900

    add mqtt

--- myproject/pybo/views/alarm_views.py
+++ myproject/pybo/views/alarm_views.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, render_template, request, url_for
 from werkzeug.utils import redirect
 from datetime import datetime
+import paho.mqtt.client as mqtt
 
 
 from pybo import db
 from pybo.models import Alarm
 from pybo.forms import AlarmForm
+
+# MQTT 브로커에 연결합니다
+broker_url = '44.202.115.227'  # MQTT 브로커의 URL을 입력하세요
+broker_port = 1883  # MQTT 브로커의 포트 번호를 입력하세요
+client = mqtt.Client()
 
 bp = Blueprint('alarm', __name__, url_prefix='/alarm')
 #alarm이라는 이름으로 블루프린트 객체를 생성
@@ -41,3 +47,32 @@ def delete(alarm_id):
     db.session.delete(alarm)
     db.session.commit()
     return redirect(url_for('main.index'))
+
+# MQTT 브로커에 연결합니다
+broker_url = '44.202.115.227'  # MQTT 브로커의 URL을 입력하세요
+broker_port = 1883  # MQTT 브로커의 포트 번호를 입력하세요
+client = mqtt.Client()
+
+# 연결 성공 시 실행됩니다
+def on_connect(client, userdata, flags, rc):
+    print('MQTT 브로커에 연결되었습니다.')
+    # 토픽을 구독합니다
+    topic = 'home/garden/fountain'  # 구독할 토픽을 입력하세요
+    client.subscribe(topic)
+    print('토픽 구독 성공:', topic)
+
+# 메시지를 수신할 때 실행됩니다
+def on_message(client, userdata, msg):
+    print('새로운 메시지 수신:', msg.topic, msg.payload.decode())
+    # 메시지를 처리하는 추가 로직을 여기에 작성하세요
+
+# 오류 발생 시 실행됩니다
+def on_error(client, userdata, msg):
+    print('MQTT 오류:', msg)
+
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_error = on_error
+
+client.connect(broker_url, broker_port, 60)
+client.loop_start()
